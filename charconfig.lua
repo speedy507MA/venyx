745524806dd6911022a03a8f7517c278da427a50
Josh <77637194+speedy507MA@users.noreply.github.com>
2025-01-03 17:08:52 -0500

    Create charconfig.lua

---
+++ charconfig.lua
@@ -0,0 +1,545 @@
+{
+    ["Caroline Forbes"] = {
+       ["Outfits"] = {
+          ["Classic Caroline"],
+          ["60's Decade Dance"],
+          ["Cheerleader Caroline"],
+          ["Winter Caroline"]
+       },
+       ["MainOutfit"] = ["Classic Caroline"]
+    },
+    ["Lucien Castle"] = {
+       ["Outfits"] = {
+          ["Classic Lucien"]
+       },
+       ["MainOutfit"] = ["Classic Lucien"]
+    },
+    ["Josh Rosza"] = {
+       ["Outfits"] = {
+          ["Classic Josh"]
+       },
+       ["MainOutfit"] = ["Classic Josh"]
+    },
+    ["Milton Greasley"] = {
+       ["Outfits"] = {
+          ["Classic Milton"]
+       },
+       ["MainOutfit"] = ["Classic Milton"]
+    },
+    ["Stefan Salvatore"] = {
+       ["Outfits"] = {
+          ["Classic Stefan"],
+          ["Stefan's Secondary Outfit"],
+          ["Football Stefan"],
+          ["1864 Stefan"],
+          ["Halloween Stefan"]
+       },
+       ["MainOutfit"] = ["Classic Stefan"]
+    },
+    ["Landon Kirby"] = {
+       ["Outfits"] = {
+          ["Classic Landon"]
+       },
+       ["MainOutfit"] = ["Classic Landon"]
+    },
+    ["Niklaus Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Niklaus"],
+          ["Formal Niklaus"]
+       },
+       ["MainOutfit"] = ["Classic Niklaus"]
+    },
+    ["Kol Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Kol"],
+          ["Formal Kol"]
+       },
+       ["MainOutfit"] = ["Classic Kol"]
+    },
+    ["Finn Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Finn"],
+          ["Formal Finn"]
+       },
+       ["MainOutfit"] = ["Classic Finn"]
+    },
+    ["Damon Salvatore"] = {
+       ["Outfits"] = {
+          ["Classic Damon"],
+          ["1864 Damon"],
+          ["Other Damon"],
+          ["Halloween Damon"]
+       },
+       ["MainOutfit"] = ["Other Damon"]
+    },
+    ["Tristan De Martel"] = {
+       ["Outfits"] = {
+          ["Classic Tristan"]
+       },
+       ["MainOutfit"] = ["Classic Tristan"]
+    },
+    ["Pearl Zhu"] = {
+       ["Outfits"] = {
+          ["Pearl Casual"]
+       },
+       ["MainOutfit"] = ["Pearl Casual"]
+    },
+    ["Elena Gilbert"] = {
+       ["Outfits"] = {
+          ["Classic Elena"],
+          ["Casual Elena"],
+          ["No Humanity Elena"],
+          ["Cheerleader Elena"],
+          ["Human Elena"],
+          ["Halloween Elena"],
+          ["Winter Elena"]
+       },
+       ["MainOutfit"] = ["Casual Elena"]
+    },
+    ["Rose"] = {
+       ["Outfits"] = {
+          ["Classic Rose"]
+       },
+       ["MainOutfit"] = ["Classic Rose"]
+    },
+    ["Katherine Pierce"] = {
+       ["Outfits"] = {
+          ["Classic Katherine"],
+          ["Katherine's Disguise"],
+          ["Original Katherine"],
+          ["Human Katherine"],
+          ["1864 Katherine"],
+          ["Halloween Katherine"]
+       },
+       ["MainOutfit"] = ["Original Katherine"]
+    },
+    ["Lorenzo “Enzo” St. John"] = {
+       ["Outfits"] = {
+          ["Classic Lorenzo"]
+       },
+       ["MainOutfit"] = ["Classic Lorenzo"]
+    },
+    ["Sebastian"] = {
+       ["Outfits"] = {
+          ["Classic Sebastian"]
+       },
+       ["MainOutfit"] = ["Classic Sebastian"]
+    },
+    ["Matt Donovan"] = {
+       ["Outfits"] = {
+          ["Classic Matt"],
+          ["Football Matt"]
+       },
+       ["MainOutfit"] = ["Classic Matt"]
+    },
+    ["Annabelle Zhu"] = {
+       ["Outfits"] = {
+          ["Classic Annabelle"]
+       },
+       ["MainOutfit"] = ["Classic Annabelle"]
+    },
+    ["Josie Saltzman"] = {
+       ["Outfits"] = {
+          ["Classic Josie"],
+          ["Josie's Old Uniform"],
+          ["Josie white top"],
+          ["Blue Josie"]
+       },
+       ["MainOutfit"] = ["Josie white top"]
+    },
+    ["Sheila Bennett"] = {
+       ["Outfits"] = {
+          ["Classic Sheila"]
+       },
+       ["MainOutfit"] = ["Classic Sheila"]
+    },
+    ["Nadia Petrova"] = {
+       ["Outfits"] = {
+          ["Classic Nadia"]
+       },
+       ["MainOutfit"] = ["Classic Nadia"]
+    },
+    ["Aya Al-Rashid"] = {
+       ["Outfits"] = {
+          ["Classic Aya"]
+       },
+       ["MainOutfit"] = ["Classic Aya"]
+    },
+    ["Lexi Branson"] = {
+       ["Outfits"] = {
+          ["Classic Lexi"]
+       },
+       ["MainOutfit"] = ["Classic Lexi"]
+    },
+    ["Lizzie Saltzman"] = {
+       ["Outfits"] = {
+          ["Classic Lizzie"],
+          ["Lizzie's Old Uniform"]
+       },
+       ["MainOutfit"] = ["Classic Lizzie"]
+    },
+    ["Carol Lockwood"] = {
+       ["Outfits"] = {
+          ["Classic Carol"]
+       },
+       ["MainOutfit"] = ["Classic Carol"]
+    },
+    ["Liz Forbes"] = {
+       ["Outfits"] = {
+          ["Classic Liz"]
+       },
+       ["MainOutfit"] = ["Classic Liz"]
+    },
+    ["Jenna Sommers"] = {
+       ["Outfits"] = {
+          ["Classic Jenna"]
+       },
+       ["MainOutfit"] = ["Classic Jenna"]
+    },
+    ["Camille O'Connell"] = {
+       ["Outfits"] = {
+          ["Classic Camille"]
+       },
+       ["MainOutfit"] = ["Classic Camille"]
+    },
+    ["Vicki Donovan"] = {
+       ["Outfits"] = {
+          ["Classic Vicki"],
+          ["Halloween Vicki"]
+       },
+       ["MainOutfit"] = ["Classic Vicki"]
+    },
+    ["Jeremy Gilbert"] = {
+       ["Outfits"] = {
+          ["Classic Jeremy"]
+       },
+       ["MainOutfit"] = ["Classic Jeremy"]
+    },
+    ["Alaric Saltzman"] = {
+       ["Outfits"] = {
+          ["Classic Alaric"]
+       },
+       ["MainOutfit"] = ["Classic Alaric"]
+    },
+    ["Alyssa Chang"] = {
+       ["Outfits"] = {
+          ["Classic Alyssa"]
+       },
+       ["MainOutfit"] = ["Classic Alyssa"]
+    },
+    ["Penelope Park"] = {
+       ["Outfits"] = {
+          ["Classic Penelope"]
+       },
+       ["MainOutfit"] = ["Classic Penelope"]
+    },
+    ["Celeste Dubois"] = {
+       ["Outfits"] = {
+          ["Classic Celeste"]
+       },
+       ["MainOutfit"] = ["Classic Celeste"]
+    },
+    ["Kai Parker"] = {
+       ["Outfits"] = {
+          ["Classic Kai"],
+          ["Formal Kai"]
+       },
+       ["MainOutfit"] = ["Formal Kai"]
+    },
+    ["Luke Parker"] = {
+       ["Outfits"] = {
+          ["Classic Luke"]
+       },
+       ["MainOutfit"] = ["Classic Luke"]
+    },
+    ["Bonnie Bennett"] = {
+       ["Outfits"] = {
+          ["Classic Bonnie"],
+          ["Bonnie Senior Year"],
+          ["Cheerleader Bonnie"],
+          ["Curly Bonnie"],
+          ["Prison world Bonnie"],
+          ["Expression Bonnie"],
+          ["Winter Bonnie"]
+       },
+       ["MainOutfit"] = ["Expression Bonnie"]
+    },
+    ["Lucy Bennett"] = {
+       ["Outfits"] = {
+          ["Classic Lucy"]
+       },
+       ["MainOutfit"] = ["Classic Lucy"]
+    },
+    ["Dark Josie Saltzman"] = {
+       ["Outfits"] = {
+          ["Dark Josie"],
+          ["Dark Josie2"],
+          ["Dark Queen"],
+          ["Blue Sweater"],
+          ["Alt Dark Josie"]
+       },
+       ["MainOutfit"] = ["Alt Dark Josie"]
+    },
+    ["Genevieve"] = {
+       ["Outfits"] = {
+          ["Classic Genevieve"]
+       },
+       ["MainOutfit"] = ["Classic Genevieve"]
+    },
+    ["Sophie Deveraux"] = {
+       ["Outfits"] = {
+          ["Classic Sophie"]
+       },
+       ["MainOutfit"] = ["Classic Sophie"]
+    },
+    ["Olivia Parker"] = {
+       ["Outfits"] = {
+          ["Classic Olivia"]
+       },
+       ["MainOutfit"] = ["Classic Olivia"]
+    },
+    ["Aiden"] = {
+       ["Outfits"] = {
+          ["Classic Aiden"]
+       },
+       ["MainOutfit"] = ["Classic Aiden"]
+    },
+    ["Jed"] = {
+       ["Outfits"] = {
+          ["Classic Jed"]
+       },
+       ["MainOutfit"] = ["Classic Jed"]
+    },
+    ["Rafael Waithe"] = {
+       ["Outfits"] = {
+          ["Classic Rafael"]
+       },
+       ["MainOutfit"] = ["Classic Rafael"]
+    },
+    ["Mason Lockwood"] = {
+       ["Outfits"] = {
+          ["Classic Mason"]
+       },
+       ["MainOutfit"] = ["Classic Mason"]
+    },
+    ["Tyler Lockwood"] = {
+       ["Outfits"] = {
+          ["Classic Tyler"],
+          ["Football Tyler"]
+       },
+       ["MainOutfit"] = ["Classic Tyler"]
+    },
+    ["Hayley Marshall"] = {
+       ["Outfits"] = {
+          ["Classic Hayley"]
+       },
+       ["MainOutfit"] = ["Classic Hayley"]
+    },
+    ["Vincent Griffith"] = {
+       ["Outfits"] = {
+          ["Classic Vincent"]
+       },
+       ["MainOutfit"] = ["Classic Vincent"]
+    },
+    ["Malcolm"] = {
+       ["Outfits"] = {
+          ["Classic Malcolm"]
+       },
+       ["MainOutfit"] = ["Classic Malcolm"]
+    },
+    ["Oscar"] = {
+       ["Outfits"] = {
+          ["Classic Oscar"]
+       },
+       ["MainOutfit"] = ["Classic Oscar"]
+    },
+    ["Nora Hildegard"] = {
+       ["Outfits"] = {
+          ["Classic Nora"],
+          ["New Nora"],
+          ["Halloween Nora"]
+       },
+       ["MainOutfit"] = ["New Nora"]
+    },
+    ["Mary Louise"] = {
+       ["Outfits"] = {
+          ["Classic Mary"],
+          ["Alt Mary"],
+          ["Halloween Mary"]
+       },
+       ["MainOutfit"] = ["Alt Mary"]
+    },
+    ["Valerie Tulle"] = {
+       ["Outfits"] = {
+          ["Classic Valerie"],
+          ["Valerie Cardigan"]
+       },
+       ["MainOutfit"] = ["Classic Valerie"]
+    },
+    ["Aurora De Martel"] = {
+       ["Outfits"] = {
+          ["Classic Aurora"],
+          ["Legacies Aurora"]
+       },
+       ["MainOutfit"] = ["Legacies Aurora"]
+    },
+    ["Sage"] = {
+       ["Outfits"] = {
+          ["Classic Sage"]
+       },
+       ["MainOutfit"] = ["Classic Sage"]
+    },
+    ["Isobel Flemming"] = {
+       ["Outfits"] = {
+          ["Classic Isobel"]
+       },
+       ["MainOutfit"] = ["Classic Isobel"]
+    },
+    ["Hope Mikaelson"] = {
+       ["Outfits"] = {
+          ["Casual Hope"],
+          ["Hope School Uniform"],
+          ["Hope Tribrid"],
+          ["Red Riding Hope"]
+       },
+       ["MainOutfit"] = ["Hope Tribrid"]
+    },
+    ["Jules"] = {
+       ["Outfits"] = {
+          ["Classic Jules"]
+       },
+       ["MainOutfit"] = ["Classic Jules"]
+    },
+    ["Finch Tarrayo"] = {
+       ["Outfits"] = {
+          ["Classic Finch"]
+       },
+       ["MainOutfit"] = ["Classic Finch"]
+    },
+    ["Jackson Kenner"] = {
+       ["Outfits"] = {
+          ["Classic Jackson"]
+       },
+       ["MainOutfit"] = ["Classic Jackson"]
+    },
+    ["Davina Claire"] = {
+       ["Outfits"] = {
+          ["Classic Davina"],
+          ["Alternate Davina"],
+          ["Elegant Davina"],
+          ["Harvest Davina"]
+       },
+       ["MainOutfit"] = ["Harvest Davina"]
+    },
+    ["Marcel Gerard"] = {
+       ["Outfits"] = {
+          ["Classic Marcel"]
+       },
+       ["MainOutfit"] = ["Classic Marcel"]
+    },
+    ["Beau"] = {
+       ["Outfits"] = {
+          ["Classic Beau"],
+          ["Beau Jacket"]
+       },
+       ["MainOutfit"] = ["Beau Jacket"]
+    },
+    ["Keelin Malraux"] = {
+       ["Outfits"] = {
+          ["Classic Keelin"]
+       },
+       ["MainOutfit"] = ["Classic Keelin"]
+    },
+    ["Rebekah Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Rebekah"],
+          ["Casual Rebekah"],
+          ["Cheerleader Rebekah"],
+          ["Curly Rebekah"],
+          ["Ball Rebekah"]
+       },
+       ["MainOutfit"] = ["Curly Rebekah"]
+    },
+    ["Elijah Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Elijah"],
+          ["Formal Elijah"]
+       },
+       ["MainOutfit"] = ["Classic Elijah"]
+    },
+    ["Mikael Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Mikael"]
+       },
+       ["MainOutfit"] = ["Classic Mikael"]
+    },
+    ["Lily Salvatore"] = {
+       ["Outfits"] = {
+          ["Classic Lily"]
+       },
+       ["MainOutfit"] = ["Classic Lily"]
+    },
+    ["Qetsiyah"] = {
+       ["Outfits"] = {
+          ["Tessa"],
+          ["Wedding Qetsiyah"],
+          ["Halloween Qetsiyah"]
+       },
+       ["MainOutfit"] = ["Wedding Qetsiyah"]
+    },
+    ["Freya Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Freya"],
+          ["Long hair Freya"]
+       },
+       ["MainOutfit"] = ["Long hair Freya"]
+    },
+    ["Josette “Jo” Parker"] = {
+       ["Outfits"] = {
+          ["Classic Josette"],
+          ["Josette Doctor"]
+       },
+       ["MainOutfit"] = ["Classic Josette"]
+    },
+    ["Monique Deveraux"] = {
+       ["Outfits"] = {
+          ["Classic Monique"],
+          ["Harvest Monique"]
+       },
+       ["MainOutfit"] = ["Harvest Monique"]
+    },
+    ["Esther Mikaelson"] = {
+       ["Outfits"] = {
+          ["Classic Esther"],
+          ["Originals Esther"],
+          ["Ball Esther"]
+       },
+       ["MainOutfit"] = ["Classic Esther"]
+    },
+    ["Dahlia"] = {
+       ["Outfits"] = {
+          ["Classic Dahlia"],
+          ["Church Dahlia"],
+          ["Brown Coat Dahlia"]
+       },
+       ["MainOutfit"] = ["Classic Dahlia"]
+    },
+    ["Josephine LaRue"] = {
+       ["Outfits"] = {
+          ["Classic Josephine"]
+       },
+       ["MainOutfit"] = ["Classic Josephine"]
+    },
+    ["Bastianna Natale"] = {
+       ["Outfits"] = {
+          ["Classic Bastianna"]
+       },
+       ["MainOutfit"] = ["Classic Bastianna"]
+    },
+    ["Agnes"] = {
+       ["Outfits"] = {
+          ["Classic Agnes"]
+       },
+       ["MainOutfit"] = ["Classic Agnes"]
+    }
+ }
